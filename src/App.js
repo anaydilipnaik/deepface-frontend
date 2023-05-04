50e4a7347da316df8618c5e6d00035545b11fdac
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2023-05-04 15:23:02 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import WebcamCapture from "./WebcamCapture";
+
+function App() {
+  return (
+    <div>
+      <h1>Webcam Capture</h1>
+      <WebcamCapture />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/WebcamCapture.js
@@ -0,0 +1,48 @@
+import React, { useState, useRef } from "react";
+
+function WebcamCapture() {
+  const [imageSrc, setImageSrc] = useState(null);
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  const handleCaptureClick = () => {
+    const context = canvasRef.current.getContext("2d");
+    context.drawImage(videoRef.current, 0, 0, 640, 480);
+    const dataUrl = canvasRef.current.toDataURL();
+    setImageSrc(dataUrl);
+    downloadImage(dataUrl);
+  };
+
+  const downloadImage = (dataUrl) => {
+    const link = document.createElement("a");
+    link.download = "webcam-image.png";
+    link.href = dataUrl;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const handleVideoStream = (stream) => {
+    videoRef.current.srcObject = stream;
+  };
+
+  const handleError = (error) => {
+    console.error(error);
+  };
+
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(handleVideoStream)
+    .catch(handleError);
+
+  return (
+    <div>
+      <video ref={videoRef} autoPlay />
+      <button onClick={handleCaptureClick}>Capture</button>
+      {imageSrc && <img src={imageSrc} alt="captured" />}
+      <canvas ref={canvasRef} style={{ display: "none" }} />
+    </div>
+  );
+}
+
+export default WebcamCapture;
